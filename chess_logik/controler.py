fd41875f4304828c2f52bcb2d0ee1c24fbcbc8d4
JaryjDementor <jaryjdementor@gmail.com>
2022-09-07 18:28:16 +0200

    upd class Game, Watcher, King

--- chess_logik/controler.py
+++ chess_logik/controler.py
@@ -1,5 +1,5 @@
 from models import Player1, Player2, Move
-from figures import get_figure, get_field, letter_to_number1
+from figures import get_figure, get_field, letter_to_number1, King, Watcher
 
 
 class Game():
@@ -18,7 +18,7 @@ class Game():
         queue = 'player1'
         color = 'white'
         color_figure = 'b'
-        king = 'w_k'
+        king = 'b_k'
         opponents_figure = None
         list_avoiding_checkmate = []
         while value_for_while == 0:
@@ -34,6 +34,7 @@ class Game():
                     if start_game[int(numeric_field[0])][int(numeric_field[-1])] == figure:
                         step = input('step - ')
                         numeric_step = letter_to_number1(step)
+                        print('du', list_avoiding_checkmate)
                         if not list_avoiding_checkmate or [figure, numeric_field,numeric_step] in list_avoiding_checkmate:
                             class_figure = get_figure(figure)(numeric_field)
                             dest_field = class_figure.validate_move(numeric_step, color, start_game)
@@ -46,6 +47,7 @@ class Game():
                                             if [opponents_figure, numeric_step] in cordinates_figures_player2:
                                                 cordinates_figures_player2.remove([opponents_figure, numeric_step])
                                                 player1.taken_figure.append(opponents_figure)
+
 
                                     queue = 'player2'
                                     color = 'black'
@@ -63,20 +65,27 @@ class Game():
                                     color = 'white'
                                     king = 'w_k'
                             start_game = game.start(cordinates_figures_player1, cordinates_figures_player2)
+                            print(queue)
                             print('oponents figure', player2.taken_figure)
                             kings_field = ''
+                            count = 0
                             for i in start_game:
                                 print(i)
                                 for j in i:
                                     if j == king:
-                                        kings_field = str(start_game.index(i)) + '.' + str(i.index(j))
+                                        kings_field = str(start_game.index(i)) + '.' + str(count)
+                                    count+=1
+                                count = 0
                             print('oponents figure', player1.taken_figure)
                             opponents_figure = None
-                            checkmate_check = King(kings_field)
-                            if checkmate_check.checkmate_check([king, kings_field], start_game):
-                                watcher = Watcher()
-                                list_avoiding_checkmate = watcher.avoiding_checkmate([king, kings_field],start_game, numeric_step)
-                                print(list_avoiding_checkmate)
+                            watcher = Watcher()
+                            dict_avalible_move_oponents = watcher.dict_avalible_move_oponents(color, start_game)
+                            for i in dict_avalible_move_oponents.values():
+                                if kings_field in i:
+                                    list_avoiding_checkmate = watcher.avoiding_checkmate([king, kings_field], start_game, numeric_step)
+                                else:
+                                    list_avoiding_checkmate = []
+
 
                 except ValueError:
                     print('Not exist field')
